1bb4675d343b566678aa576b7c7d405c57dcca8c
Luciamarinm <luciamarinmartinez94@gmail.com>
2024-03-05 17:52:04 +0100

    Variables

--- externo.js
+++ externo.js
@@ -1,2 +1,25 @@
+//let se usa
+//Declaración explicita
 let c = 2;
 let d = 4;
+//Declaración implicita, no utiliza var, let o const
+edadPersona = 26;
+//es obligatorio darle valor desde un inicio
+
+
+/*
+Tipos de datos:
+-Primitivos
+    -Números
+    -Texto(strings)
+    -Booleanos
+    -null
+    -undefined
+-Objetos
+    -Arrays
+.Symbol(ES6)
+
+var es para declarar una variable de manera global
+let se delimita con {}
+const tiene un valor que no se puede modificar, los nombres de las variables deben ir en mayúsculas
+*/
